13d9e0db990ed3de3ee6ed8a7a7a97af382a66a3
xeavier <38816671+xeavier@users.noreply.github.com>
2020-05-26 23:15:51 +0530

    Update image.service.ts

--- src/app/shared/image.service.ts
+++ src/app/shared/image.service.ts
@@ -6,13 +6,32 @@ import { Global } from '../providers/global';
 })
 export class ImageService {
   public mierda: any;
+  public page: number:
+  public perPage:number:
+  public orderBy: string:
 
   constructor(private global: Global) { }
+
+  getlist(page, perPage, orderBy){
+   return this.global.UNSPLASH_API.photos.listPhotos(page, perPage, orderBy).then(response => response.json()).then(json => json.results);
+  } 
+
+ ///////////////
 
   searchImages(userInput: String, pageIndex:number = 1, elementsPerPage:number = 30): Promise<any> {
   return this.global.UNSPLASH_API.search.photos(userInput, pageIndex, elementsPerPage).then(response => response.json()).then(json => json.results);
     
   }
+
+  getcollection(userInput: String, pageIndex:number = 1, elementsPerPage:number = 30){
+   return this.global.UNSPLASH_API.search.collections(userInput, pageIndex, elementsPerPage).then(response => response.json()).then(json => json.results);
+  }
+
+  getuser(userInput: String, pageIndex:number = 1, elementsPerPage:number = 30){
+   return this.global.UNSPLASH_API.search.users(userInput, pageIndex, elementsPerPage).then(response => response.json()).then(json => json.results);
+  }
+
+  //////////////////////
 
   getImageById(id: String, width: number, height:number, rectangle:number) : Promise<any>{
     return this.global.UNSPLASH_API.photos.getPhoto(id, width, height, rectangle).then(response => response.json()).then(json => json.results);;
